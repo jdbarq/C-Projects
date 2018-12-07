a44c081fd0ce5bc352dfd970fb955080b8780d5a
James Barquera <jamesbarquera@gmail.com>
2018-12-06 17:07:23 -0800

    Create Database.h

---
+++ project1-folder/Database.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include "Employee.h"
+
+namespace Records {
+
+	const int kFirstEmployeeNumber = 1000; 
+
+
+
+	class Database {
+	public:
+		Employee& addEmployee(const std::string& firstName,
+			const std::string& lastName); 
+		Employee& getEmployee(int employeeNumber);
+		Employee& getEmployee(const std::string& firstName,
+			const std::string& lastName);
+
+		void displayAll() const; 
+		void displayCurrent() const; 
+		void displayFormer() const; 
+
+	private: 
+		std::vector<Employee> mEmployees; 
+		int mNextEmployeeNumber = kFirstEmployeeNumber; 
+
+
+
+
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
